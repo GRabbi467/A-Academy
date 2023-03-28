a5f07302aece5d9d19bc5dd10ae868bfb64c3b98
Golam Rabbi <57221660+GRabbi467@users.noreply.github.com>
2023-03-29 05:28:28 +0600

    Header done

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -3,7 +3,7 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <p>This is about page</p>
+            <h1>This is About Page</h1>
         </div>
     );
 };
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,18 +1,39 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import './Header.css'
 import { Link } from 'react-router-dom';
+import Image from 'react-bootstrap/Image';
+import logo from '../free-logo2.svg'
+
 
 const Header = () => {
     return (
-        <div>
-            <nav>
-                 
-                <Link to = '/'>Home</Link>
-                <Link to = '/course'>Courses</Link>
-                <Link to = '/blog'>Blog</Link>
-                <Link to = '/faq'>FAQ</Link>
-                <Link to = '/about'>About</Link>
-            </nav>
-        </div>
+    <nav className='header'>
+
+    <div className="logo-title">
+    <img src={logo} alt="" />
+    <p className='title'>A+ Academy</p>
+    </div>
+
+
+    <div className="header-links">
+           <Link to = '/'>Home</Link>
+           <Link to = '/course'>Courses</Link>
+           <Link to = '/blog'>Blog</Link>
+           <NavDropdown
+              id="nav-dropdown-dark-example"
+              title="FAQ"
+              menuVariant="dark"
+            >
+              <NavDropdown.Item Link to="#">Action</NavDropdown.Item>
+              <NavDropdown.Item Link to="#">Another action</NavDropdown.Item>
+              <NavDropdown.Item Link to="#">Something</NavDropdown.Item>
+            </NavDropdown>
+           <Link to = '/about'>About</Link>
+    </div>
+
+    </nav> 
     );
 };
 
